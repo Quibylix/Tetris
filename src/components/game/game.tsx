d59613af06da79513905d08b7ed6b9313969f6b3
Vanaldito <barraza.fredimanuel@gmail.com>
2023-12-18 20:53:54 -0600

    refactor: move board drawing logic to the Board class

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef } from "react";
+import {
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  TETRIS_HORIZONTAL_BLOCKS,
+  TETRIS_VERTICAL_BLOCKS,
+} from "./constants";
 import styles from "./game.module.css";
 import { Board } from "./logic";
-
-const TETRIS_HORIZONTAL_BLOCKS = 10;
-const TETRIS_VERTICAL_BLOCKS = 20;
-const BLOCK_SIZE = 32;
-const CANVAS_WIDTH = TETRIS_HORIZONTAL_BLOCKS * BLOCK_SIZE;
-const CANVAS_HEIGHT = TETRIS_VERTICAL_BLOCKS * BLOCK_SIZE;
 
 export default function Game() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -19,29 +19,15 @@ export default function Game() {
 
     if (!ctx) return;
 
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
     const board = new Board(TETRIS_VERTICAL_BLOCKS, TETRIS_HORIZONTAL_BLOCKS);
-
-    for (let row = 0; row < board.rows; row++) {
-      for (let col = 0; col < board.cols; col++) {
-        ctx.strokeStyle = "#fff";
-
-        ctx.strokeRect(
-          col * BLOCK_SIZE,
-          row * BLOCK_SIZE,
-          BLOCK_SIZE,
-          BLOCK_SIZE,
-        );
-      }
-    }
+    board.draw(ctx);
   }, []);
 
   return (
     <canvas
       className={styles.canvas}
       data-testid="game-canvas"
+      tabIndex={0}
       ref={canvasRef}
       width={CANVAS_WIDTH}
       height={CANVAS_HEIGHT}
--- src/components/game/logic/Board.ts
+++ src/components/game/logic/Board.ts
@@ -1,3 +1,5 @@
+import { BLOCK_SIZE, CANVAS_HEIGHT, CANVAS_WIDTH } from "../constants";
+
 export class Board {
   rows: number;
   cols: number;
@@ -5,5 +7,23 @@ export class Board {
   constructor(rows: number, cols: number) {
     this.rows = rows;
     this.cols = cols;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        ctx.strokeStyle = "#fff";
+
+        ctx.strokeRect(
+          col * BLOCK_SIZE,
+          row * BLOCK_SIZE,
+          BLOCK_SIZE,
+          BLOCK_SIZE,
+        );
+      }
+    }
   }
 }
